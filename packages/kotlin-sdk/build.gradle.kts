988d16fb3f7b0c464bc626c0be48664aab8f9214
radupana <radupana@ymail.com>
2025-11-30 12:44:57 +0000

    Bump version to 0.1.2

--- packages/kotlin-sdk/build.gradle.kts
+++ packages/kotlin-sdk/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "io.github.radupana"
-version = "0.1.1"
+version = "0.1.2"
 
 mavenPublishing {
     publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
